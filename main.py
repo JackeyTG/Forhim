6c7f284e0c8116e7d36b115358f75e7543fac9fe
Rose <123132237+Rosestorycity@users.noreply.github.com>
2023-04-27 12:37:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "18401114"
+API_HASH = "e9105cffc9ef49b4011dfeb843acb091"
+BOT_TOKEN = "5691494843:AAF_iYfZHfFTZToJn2hU33Ywc-N9yTllu1w"
 
 
 SIJITH = Client(
