6f490d1ffee87b26f0790854e4ca7361dbb40af8
Rose <123132237+Rosestorycity@users.noreply.github.com>
2023-04-27 12:33:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+SIJITH = Client(
+    name="filetolinkbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot Was Started")
+
+
+SIJITH.run()
